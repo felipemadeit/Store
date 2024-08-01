78600cb564e74da548c86b3700e7563f688fecff
felipemadeit <felipemadeit@gmail.com>
2024-07-31 19:55:19 -0500

    Se modifico la manera en la que se sirven los archivos estaticos

--- core/settings.py
+++ core/settings.py
@@ -156,3 +156,10 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'store/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
